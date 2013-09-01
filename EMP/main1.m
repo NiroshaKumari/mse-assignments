967a948bcbb4a70ea5686f3a93cf8d1fa6320285
NiroshaKumari <niroshagangolli@gmail.com>
2013-09-01 11:16:06 +0500

    Update main1.m

--- EMP/main1.m
+++ EMP/main1.m
@@ -5,9 +5,9 @@
 int main()
 {
 	employee* e=[[employee alloc]init];		//allocating and initialization of the object.
-	[e setName:@"Sristy"];
+	[e setName:@"NIROSHA"];
 	[e setDept:@"ISE"];
-	[e setIdno:@"1PI10IS047"];
+	[e setIdno:@"1PI10IS067"];
 	[e print];
 	[e release];			//free the memory allocated.
 	return 0;
